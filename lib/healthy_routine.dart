83aaaac5760c6c4c6037ce123e60380f78661db9
mugbug <zaronipedro@outlook.com>
2020-04-25 19:01:49 -0300

    Create mocked home page

--- lib/healthy_routine.dart
+++ lib/healthy_routine.dart
@@ -1,2 +1,8 @@
+export 'package:healthy_routine_mobile/presentation/components/bullet.dart';
+export 'package:healthy_routine_mobile/presentation/components/task_card.dart';
 export 'package:healthy_routine_mobile/presentation/daily_task_list_page.dart';
 export 'package:healthy_routine_mobile/services/database/task_database_service.dart';
+export 'package:healthy_routine_mobile/styles/colors.dart';
+export 'package:healthy_routine_mobile/styles/shapes.dart';
+export 'package:healthy_routine_mobile/styles/text_style.dart';
+export 'package:healthy_routine_mobile/utils/extensions/widget_extensions.dart';
---
+++ lib/styles/shapes.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+
+const ShapeBorder ROUNDED_HEADER_SHAPE = RoundedRectangleBorder(
+  borderRadius: BorderRadius.all(Radius.circular(20)),
+);
